9316f94e4ad981a51639c98918f7776b35e07ea3
Katelynn Emerson <ke101@wellesley.edu>
2024-01-17 10:13:30 -0500

    added Building file

---
+++ Building.java
@@ -0,0 +1,23 @@
+public class Building {
+    private Room[] rooms;
+    private String title;
+    private int roomCount;
+
+    public Building(Room[] roomLayout, String name) {
+        title = name;
+        rooms = roomLayout;
+        roomCount = rooms.length;
+    }
+    
+    public void addRoom(Room r) {
+        if (roomCount>=rooms.length) {
+            Room[] temp = new Room[rooms.length*2];
+            for (int i=0; i<rooms.length; i++) {
+                temp[i] = rooms[i];
+            }
+            rooms = temp;
+        }
+        rooms[roomCount] = r;
+        roomCount++;
+    }
+}
